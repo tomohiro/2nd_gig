6bc700fd6e36d5814ae5bdc6dd7b030baf32fe1d
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-07-25 23:39:18 +0900

    Support Slop v4.2.0

--- lib/2nd_gig/server.rb
+++ lib/2nd_gig/server.rb
@@ -9,18 +9,19 @@ module SecondGig
     end
 
     def parse_options
-      opts = Slop.parse(help: true) do
-        banner 'Usage: 2nd_gig [options]'
-        on :p, :port,    'Port number to listen (default: 16705)',                 argument: :optional, as: :integer, default: 16705
-        on :s, :server,  'Host name or IP address to listen (default: localhost)', argument: :optional, as: :string,  default: :localhost
-        on :l, :log,     'Log file (default: STDOUT)',                             argument: :optional, as: :string,  default: nil
-        on :v, :version, 'Print the version' do
-          puts SecondGig::VERSION
+     opts = Slop.parse do |o|
+        o.integer '-p', '--port', 'Port number to listen (default: 16705)', default: 16705
+        o.string '-h', '--host',  'Host name or IP address to listen (default: 0.0.0.0)', default: '0.0.0.0'
+        o.string '-l', '--log',   'Log file (default: STDOUT)', default: nil
+        o.on '-v', '--version', 'Print the version' do
+          puts InterestIrcGateway::VERSION
           exit
         end
-      end
-
-      exit if opts.present?(:help)
+        o.on '--help' do
+          puts o
+          exit
+        end
+     end
 
       logger = Logger.new(opts[:log] || STDOUT, 'daily')
       logger.level = Logger::DEBUG
